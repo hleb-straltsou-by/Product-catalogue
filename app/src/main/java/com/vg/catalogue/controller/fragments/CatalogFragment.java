a073644c7262702c5386165f4510928adc15d163
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-04 18:23:08 +0300

    add class CatalogFragment

---
+++ app/src/main/java/com/vg/catalogue/controller/fragments/CatalogFragment.java
@@ -0,0 +1,19 @@
+package com.vg.catalogue.controller.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vg.catalogue.R;
+
+public class CatalogFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_catalog, container, false);
+        return v;
+    }
+}
