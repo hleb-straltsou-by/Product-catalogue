1d180b7d5d9130e5ff6a4a35d86b5a797f7c22e0
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-07 22:41:36 +0300

    update clss ActiveSubstanceEntry

--- app/src/main/java/com/vg/catalogue/dao/entries/ActiveSubstanceEntry.java
+++ app/src/main/java/com/vg/catalogue/dao/entries/ActiveSubstanceEntry.java
@@ -1,8 +1,18 @@
 package com.vg.catalogue.dao.entries;
 
-/**
- * Created by ka1ok on 07.08.2017.
- */
-
 public class ActiveSubstanceEntry {
+
+    public static final String TABLE_NAME_HERBICIDES = "active_substance_herbicides";
+
+    public static final String TABLE_NAME_FUNGICIDES = "active_substance_fungicides";
+
+    public static final String TABLE_NAME_INSECTICIDES = "active_substance_insecticides";
+
+    public static final String TABLE_NAME_GROWTH_REGULATORS = "active_substance_growth_regulators";
+
+    public static final String TABLE_NAME_HERBICIDES_PAV = "active_substance_herbicides_pav";
+
+    public static final String COLUMN_NAME_ENTRY_ID = "active_substance_id";
+
+    public static final String COLUMN_NAME_ACTIVE_SUBSTANCE_NAME = "active_substance_name";
 }
