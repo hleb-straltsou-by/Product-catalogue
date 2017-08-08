513c4890b2fb18ee2a153c2e4cd723799ad5a630
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-08 23:47:59 +0300

    update class MiniSearchFragment

--- app/src/main/java/com/vg/catalogue/controller/fragments/MiniSearchFragment.java
+++ app/src/main/java/com/vg/catalogue/controller/fragments/MiniSearchFragment.java
@@ -1,17 +1,26 @@
 package com.vg.catalogue.controller.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.vg.catalogue.R;
+import com.vg.catalogue.controller.activities.MainActivity;
+import com.vg.catalogue.dao.implementations.SQLiteProductDao;
+import com.vg.catalogue.dao.interfaces.ProductDao;
 
 public class MiniSearchFragment extends Fragment {
 
-    private SearchView mSearchView;
+    private EditText mSearchEditText;
+
+    private ImageView mSearchImageView;
+
+    private ProductDao mProductDao;
 
     public static MiniSearchFragment newInstance() {
         return new MiniSearchFragment();
@@ -27,11 +36,24 @@ public class MiniSearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_mini_search, container, false);
 
-        mSearchView = (SearchView) v.findViewById(R.id.search_view);
-        mSearchView.setOnSearchClickListener(new View.OnClickListener(){
+        mProductDao = SQLiteProductDao.getInstance(getActivity().getApplicationContext());
+
+        mSearchEditText = (EditText) v.findViewById(R.id.mini_search_edit_view);
+        mSearchEditText.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                // searching
+                mSearchEditText.setText("");
+            }
+        });
+
+        mSearchImageView = (ImageView) v.findViewById(R.id.mini_search_image_button);
+        mSearchImageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String filterPatternName = mSearchEditText.getText().toString();
+                Intent intent = MainActivity.newIntent(getActivity(), CatalogFragment.class,
+                        filterPatternName);
+                startActivity(intent);
             }
         });
 
