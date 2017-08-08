95f08459ad1658b5bd830526a4b9d0d792b8d62c
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-08 23:46:46 +0300

    update class ProductCursorWrapper

--- app/src/main/java/com/vg/catalogue/dao/utils/ProductCursorWrapper.java
+++ app/src/main/java/com/vg/catalogue/dao/utils/ProductCursorWrapper.java
@@ -6,8 +6,6 @@ import android.database.CursorWrapper;
 import com.vg.catalogue.dao.entries.ProductEntry;
 import com.vg.catalogue.model.Product;
 
-import java.util.UUID;
-
 public class ProductCursorWrapper extends CursorWrapper {
 
     public ProductCursorWrapper(Cursor cursor){
@@ -15,16 +13,12 @@ public class ProductCursorWrapper extends CursorWrapper {
     }
 
     public Product getProduct(){
-        String uuidStr = getString(
-                getColumnIndex(ProductEntry.COLUMN_NAME_ENTRY_ID));
         String name = getString(
                 getColumnIndex(ProductEntry.COLUMN_NAME_NAME));
         String allNames = getString(
                 getColumnIndex(ProductEntry.COLUMN_NAME_ALL_NAMES));
-        String consumptionRate = getString(
-                getColumnIndex(ProductEntry.COLUMN_NAME_CONSUMPTION_RATE));
-        String processedCultures = getString(
-                getColumnIndex(ProductEntry.COLUMN_NAME_PROCESSED_CULTURES));
+        String consumptionRateAndProcessedCultures = getString(
+                getColumnIndex(ProductEntry.COLUMN_NAME_CONSUMPTION_RATE_AND_PROCESSED_CULTURES));
         String harmfulOrganismDisease = getString(
                 getColumnIndex(ProductEntry.COLUMN_NAME_HARMFUL_ORGANISM_DISEASE));
         String operatingPrinciple = getString(
@@ -36,11 +30,10 @@ public class ProductCursorWrapper extends CursorWrapper {
         int activeSubstanceId = getInt(
                 getColumnIndex(ProductEntry.COLUMN_NAME_ACTIVE_SUBSTANCE_ID));
 
-        Product product = new Product(UUID.fromString(uuidStr));
+        Product product = new Product();
         product.setName(name);
         product.setAllNames(allNames);
-        product.setConsumptionRate(consumptionRate);
-        product.setProcessedCultures(processedCultures);
+        product.setConsumptionRateAndProcessedCultures(consumptionRateAndProcessedCultures);
         product.setHarmfulOrganismOrDisease(harmfulOrganismDisease);
         product.setOperatingPrinciple(operatingPrinciple);
         product.setDaysTillLastHarvest(daysTillLastHarvest);
