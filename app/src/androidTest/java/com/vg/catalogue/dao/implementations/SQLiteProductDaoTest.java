a9ad1416b8fe8aa265c1e3081bd7f6a1081468bb
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-08 23:46:13 +0300

    update class SQLiteProductDaoTest

--- app/src/androidTest/java/com/vg/catalogue/dao/implementations/SQLiteProductDaoTest.java
+++ app/src/androidTest/java/com/vg/catalogue/dao/implementations/SQLiteProductDaoTest.java
@@ -5,10 +5,11 @@ import android.support.test.InstrumentationRegistry;
 
 import com.vg.catalogue.dao.interfaces.ProductDao;
 import com.vg.catalogue.enums.ProductCategoryEnum;
+import com.vg.catalogue.model.ActiveSubstance;
 import com.vg.catalogue.model.Product;
 
-import org.junit.Before;
 import org.junit.Test;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -21,16 +22,6 @@ public class SQLiteProductDaoTest {
     public SQLiteProductDaoTest(){
         appContext = InstrumentationRegistry.getTargetContext();
         mProductDao = SQLiteProductDao.getInstance(appContext);
-    }
-
-    @Before
-    public void deleteAllProductsFromHerbicidesTest() throws Exception {
-        ProductCategoryEnum categoryEnum = ProductCategoryEnum.HERBICIDES;
-
-        String errorMessage = "Expected size of result list equal to 0";
-
-        mProductDao.deleteAllProducts(categoryEnum);
-        assertEquals(errorMessage, 0, mProductDao.getAllProducts(categoryEnum).size());
     }
 
     @Test
@@ -61,7 +52,9 @@ public class SQLiteProductDaoTest {
         mProductDao.addProduct(product, categoryEnum);
 
         String errorMessage = "Added product object is not correspond to retrieved moment object";
-        assertEquals(errorMessage, product, mProductDao.getProduct(product.getId(), categoryEnum));
+        assertEquals(errorMessage, product.getName(),
+                mProductDao.getProduct(product.getName(), categoryEnum).get(0).getName());
+        mProductDao.deleteProduct(product.getName(), categoryEnum);
     }
 
     @Test
@@ -73,38 +66,74 @@ public class SQLiteProductDaoTest {
         mProductDao.addProduct(product, categoryEnum);
         product.setName("Test name");
 
-        mProductDao.deleteProduct(product.getId(), categoryEnum);
+        mProductDao.deleteProduct(product.getName(), categoryEnum);
         String errorMessage = "After deleting product object however the same object exists in storage";
 
-        Product resultProduct = mProductDao.getProduct(product.getId(), categoryEnum);
+        Product resultProduct = mProductDao.getProduct(product.getName(), categoryEnum).get(0);
         assertEquals(errorMessage, resultProduct.getName(), "");
-    }
-
-    @Test
-    public void updateProductInHerbicidesTest() throws Exception {
-        Product product = new Product();
-        product.setActiveSubstanceId(1);
-        ProductCategoryEnum categoryEnum = ProductCategoryEnum.HERBICIDES;
-
-        mProductDao.addProduct(product, categoryEnum);
-        product.setName("Test name");
-
-        mProductDao.updateProduct(product.getId(), product, categoryEnum);
-
-        String errorMessage = "After updating product object however" +
-                " doesn't correspond to updated product object";
-        assertEquals(errorMessage, product, mProductDao.getProduct(product.getId(), categoryEnum));
     }
 
     @Test
     public void getNotExistedProductFromHerbicidesTest() throws Exception {
         Product product = new Product();
-        product.setActiveSubstanceId(1);
         ProductCategoryEnum categoryEnum = ProductCategoryEnum.HERBICIDES;
 
         String errorMessage = "Expected empty object but has been retrieved actual object" +
                 " after query for not existing object";
-        Product resultProduct = mProductDao.getProduct(product.getId(), categoryEnum);
+        Product resultProduct = mProductDao.getProduct(product.getName(), categoryEnum).get(0);
         assertEquals(errorMessage, resultProduct.getName(), "");
+    }
+
+    @Test
+    public void findExistedProductsFromHerbicidesTest() throws Exception {
+        ProductCategoryEnum categoryEnum = ProductCategoryEnum.HERBICIDES;
+        String testName1 = "xxx";
+        String testName2 = "xxxx";
+        String testName3 = "xxxxx";
+        String testName4 = "xxtx";
+        String testName5 = "xx x";
+
+        Product product = new Product();
+        product.setName(testName1);
+        mProductDao.addProduct(product, categoryEnum);
+        product.setName(testName2);
+        mProductDao.addProduct(product, categoryEnum);
+        product.setName(testName3);
+        mProductDao.addProduct(product, categoryEnum);
+        product.setName(testName4);
+        mProductDao.addProduct(product, categoryEnum);
+        product.setName(testName5);
+        mProductDao.addProduct(product, categoryEnum);
+
+        String errorMessage = "Size of result list with products doesn't correspond to expected";
+        String namePattern = "xxx";
+        int expectedListSize = 3;
+        List<Product> resultList = mProductDao.findProducts(namePattern, categoryEnum);
+        assertEquals(errorMessage, expectedListSize, resultList.size());
+
+        // remove added products
+        mProductDao.deleteProduct(testName1, categoryEnum);
+        mProductDao.deleteProduct(testName2, categoryEnum);
+        mProductDao.deleteProduct(testName3, categoryEnum);
+        mProductDao.deleteProduct(testName4, categoryEnum);
+        mProductDao.deleteProduct(testName5, categoryEnum);
+    }
+
+    @Test
+    public void getAllHerbicidesActiveSubstancesTest() throws Exception {
+        String errorMessage = "Size of result list with active substances doesn't correspond to expected";
+        List<ActiveSubstance> substances = mProductDao.getAllActiveSubstances(
+                ProductCategoryEnum.ACTIVE_SUBSTANCES_HERBICIDES);
+        int expectedSizeOfList = 115;
+        assertEquals(errorMessage, expectedSizeOfList, substances.size());
+    }
+
+    @Test
+    public void getAllHerbicidesTest() throws Exception {
+        String errorMessage = "Size of result list with products doesn't correspond to expected";
+        List<Product> products = mProductDao.getAllProducts(
+                ProductCategoryEnum.HERBICIDES);
+        int expectedSizeOfList = 25;
+        assertEquals(errorMessage, expectedSizeOfList, products.size());
     }
 }
