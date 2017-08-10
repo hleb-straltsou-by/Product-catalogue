8543ca6ade40fb26fc727d7bdb602a3ff122ca2c
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-10 23:32:03 +0300

    update class CatalogFragment

--- app/src/main/java/com/vg/catalogue/controller/fragments/CatalogFragment.java
+++ app/src/main/java/com/vg/catalogue/controller/fragments/CatalogFragment.java
@@ -2,14 +2,11 @@ package com.vg.catalogue.controller.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.vg.catalogue.R;
-import com.vg.catalogue.controller.activities.MainActivity;
 
 public class CatalogFragment extends Fragment{
 
@@ -17,16 +14,6 @@ public class CatalogFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_catalog, container, false);
-
-        FragmentManager fm = getActivity().getSupportFragmentManager();
-
-        // add all fragments to fragment_container in transaction
-        FragmentTransaction transaction = fm.beginTransaction();
-        transaction.add(R.id.fragment_catalog_container , MiniSearchFragment.newInstance());
-        transaction.add(R.id.fragment_catalog_container , ProductListFragment.newInstance(
-                MainActivity.filterPatternNameParam));
-        transaction.commit();
-
         return v;
     }
 }
