8ede8b8ab60666e1d9c86dc9d1122ab0be4222dd
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-24 22:49:14 +0300

    update class MiniSearchFragment

--- app/src/main/java/com/vg/catalogue/controller/fragments/MiniSearchFragment.java
+++ app/src/main/java/com/vg/catalogue/controller/fragments/MiniSearchFragment.java
@@ -3,19 +3,25 @@ package com.vg.catalogue.controller.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.vg.catalogue.R;
 import com.vg.catalogue.controller.activities.MainActivity;
+import com.vg.catalogue.enums.ProductCategoryEnum;
 
 public class MiniSearchFragment extends Fragment {
 
     private SearchView mSearchView;
 
-    private boolean isReadyToSearch;
+    private AppCompatSpinner mCategorySpinner;
+
+    private ProductCategoryEnum category = ProductCategoryEnum.HERBICIDES;
 
     public static MiniSearchFragment newInstance() {
         return new MiniSearchFragment();
@@ -31,14 +37,13 @@ public class MiniSearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_mini_search, container, false);
 
-        isReadyToSearch = false;
         mSearchView = (SearchView) v.findViewById(R.id.mini_search_view);
         mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
             @Override
             public boolean onQueryTextSubmit(String query) {
                 String filterPatternName = mSearchView.getQuery().toString();
                 Intent intent = MainActivity.newIntent(getActivity(), CatalogFragment.class,
-                        filterPatternName);
+                        filterPatternName, category);
                 startActivity(intent);
                 return true;
             }
@@ -54,9 +59,28 @@ public class MiniSearchFragment extends Fragment {
             public boolean onClose() {
                 String filterPatternName = mSearchView.getQuery().toString();
                 Intent intent = MainActivity.newIntent(getActivity(), CatalogFragment.class,
-                        filterPatternName);
+                        filterPatternName, category);
                 startActivity(intent);
                 return true;
+            }
+        });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_spinner_dropdown_item,
+                ProductCategoryEnum.getProductCategoriesStrings(getActivity()));
+
+        mCategorySpinner = (AppCompatSpinner) v.findViewById(R.id.mini_search_category_spinner);
+        mCategorySpinner.setAdapter(adapter);
+        mCategorySpinner.setSelection(0);
+        mCategorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                category = ProductCategoryEnum.
+                        getProductCategoryAccordingSpinnerPossition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
             }
         });
 
